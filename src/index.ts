b20eed13e61c5dbcedf56fc694f03f925efbb3d2
shubhangi76779 <shubhangirathore3770@gmail.com>
2025-07-14 00:27:55 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -66,10 +66,10 @@ app.use("/comments", commentsRoutes);
 app.use(errorMiddleware);
 
 // Serve frontend build
-app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'dist')));
+app.use(express.static(path.join(__dirname, '..', 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '..','dist', 'index.html'));
 });
 
 // ✅ Start server
